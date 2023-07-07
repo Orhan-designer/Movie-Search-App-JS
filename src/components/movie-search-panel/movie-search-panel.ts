2041c623d6bea6a2ba174bcaa9431f0441557602
Orhan <just.hanmamedov@gmail.com>
2023-07-07 17:37:29 +0300

    feat: implement getMovies() method that return list of random movies without any options

--- src/components/movie-search-panel/movie-search-panel.ts
+++ src/components/movie-search-panel/movie-search-panel.ts
@@ -1,10 +1,11 @@
 import './movie-search-panel.scss';
-import { MovieList } from '../movie-list/movie-list';
+// import { MovieList } from '../movie-list/movie-list';
 import { MOVIE_SEARCH_PANEL_TEMPLATE } from './movie-search-panel.template';
 
 export class MovieSearchPanel {
   private container = document.querySelector('#movie-search-panel');
-  private apiKey = '53020af1';
+  private movieList = document.querySelector('#movie-list');
+  private apiKey = '44de58bc-8f6d-46e9-bb13-4148aafc51fa';
 
   public loadTemplate(): void {
     if (this.container) {
@@ -15,26 +16,45 @@ export class MovieSearchPanel {
 
   public getMovie(): void {
     const title = document.querySelector('#title') as HTMLInputElement;
-    const url = `http://www.omdbapi.com/?t=${title.value}&apikey=${this.apiKey}`;
-    const movieListComponent = new MovieList();
+    // const movieListComponent = new MovieList();
 
-    if (!title.value?.length) {
-      movieListComponent.loadTemplate({
-        template: `<h3 class="msg">Please Enter A Movie Name</h3>`
-      });
-      return;
-    }
+    // if (!title.value?.length) {
+    //   movieListComponent.loadTemplate({
+    //     template: `<h3 class="msg">Please Enter A Movie Name</h3>`
+    //   });
+    //   return;
+    // }
 
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => {
-        if (data.Response === 'True') {
-          movieListComponent.loadTemplate({ data });
+    fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films', {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': this.apiKey,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(json => {
+        for (const item of json.items) {
+          const div = document.createElement('div');
+          div.className = 'info';
+          div.innerHTML = `
+            <div>
+              <img src=${item.posterUrl} class="poster">
+            </div>
+            <div>
+              <h2>${item.nameOriginal}</h2>
+              <div class="rating">
+                  <p><b>Rating</b>: ${item.ratingImdb}</p>
+              </div>
+            </div>
+            <div class="sub-info">
+              <p><b>Year</b>: ${item.year}</p>
+            </div>`;
+
+          this.movieList?.append(div);
         }
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .catch(err => console.log(err));
   }
 
   private addListenerToSearchButton(): void {
