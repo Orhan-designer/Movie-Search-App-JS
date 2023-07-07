636c48fecfc23eab20b5c1464d7a939adabafda6
Orhan <just.hanmamedov@gmail.com>
2023-07-07 10:43:25 +0300

    fix: rename class to Movie, add params to Movie class and use them in getMovie method, add helpers foulder where i can keep url and key in object about movie site, add type module in script tag for export and import files

---
+++ helpers/moviesOptions.js
@@ -0,0 +1,5 @@
+export const options = {
+    url: 'http://www.omdbapi.com/?t=',
+    apiKey: '53020af1'
+}
+
--- js/index.js
+++ js/index.js
@@ -1,46 +1,51 @@
+import { options } from '../helpers/moviesOptions.js';
 const title = document.getElementById('title');
 const searchBtn = document.getElementById('search-by-title-button');
 const showResults = document.getElementById('search-by-title-response');
-const key = '53020af1';
 
-class getMovies {
-    constructor() { }
+class Movie {
+    constructor(movieName, url, apiKey) {
+        this.movieName = movieName;
+        this.url = url;
+        this.apiKey = apiKey;
+    }
 
-    async getMovie() {
-        const movieName = title.value;
-        const url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    async getMovies() {
+        this.movieName = title.value;
+        this.apiKey = options.apiKey;
+        this.url = `${options.url}${this.movieName}&apikey=${options.apiKey}`;
 
-        if (movieName.length <= 0) {
-            showResults.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
+        if (this.movieName.length <= 0) {
+            showResults.innerHTML = '<h3 class="msg">Please Enter A Movie Name</h3>';
             return;
         }
 
-        await fetch(url)
+        await fetch(this.url)
             .then((resp) => resp.json())
             .then((data) => {
                 if (data.Response === "True") {
                     showResults.innerHTML = `
-                    <div class="info">
-                        <img src=${data.Poster} class="poster">
-                        <div>
-                            <h2>${data.Title}</h2>
-                            <div class="rating">
-                                <p><b>Rating</b>: ${data.imdbRating}</p>
+                        <div class="info">
+                            <img src=${data.Poster} class="poster">
+                            <div>
+                                <h2>${data.Title}</h2>
+                                <div class="rating">
+                                    <p><b>Rating</b>: ${data.imdbRating}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="sub-info">
-                        <p><b>Year</b>: ${data.Year}</p>
-                        <p><b>Actors</b>: ${data.Actors}</p>
-                        <p><b>Plot</b>: ${data.Plot}</p>
-                    </div>`;
+                        <div class="sub-info">
+                            <p><b>Year</b>: ${data.Year}</p>
+                            <p><b>Actors</b>: ${data.Actors}</p>
+                            <p><b>Plot</b>: ${data.Plot}</p>
+                        </div>`;
                 }
             })
             .catch((error) => {
                 console.log(error);
             });
     }
-}
 
-searchBtn.addEventListener('click', new getMovies().getMovie);
-showResults.addEventListener('load', getMovies);
+}
+searchBtn.addEventListener('click', new Movie(title.value, options.url, options.apiKey).getMovies);
+showResults.addEventListener('load', new Movie().getMovies);
