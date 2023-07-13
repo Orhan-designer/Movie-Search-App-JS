1c39c06bb16021bb0d7030c0bf2dd5f543a70f91
Orhan <just.hanmamedov@gmail.com>
2023-07-13 10:51:50 +0300

    feat: Persist search parameters in storage

--- src/components/movie-search-panel/movie-search-panel.template.ts
+++ src/components/movie-search-panel/movie-search-panel.template.ts
@@ -3,20 +3,20 @@ export const MOVIE_SEARCH_PANEL_TEMPLATE = `
     <div class="sub-container">
       <label class="control-label" for="keyword">Title:</label>
       <input type="text" id="keyword" name="keyword" class="input-small" />
-      <label class="control-label" for="year-from">Year from:</label>
-      <input type="text" id="year-from" name="year-from" class="input-small" />
-      <label class="control-label" for="year-to">Year to:</label>
-      <input type="text" id="year-to" name="year-to" class="input-small" />
+      <label class="control-label" for="yearFrom">Year from:</label>
+      <input type="text" id="yearFrom" name="yearFrom" class="input-small" />
+      <label class="control-label" for="yearTo">Year to:</label>
+      <input type="text" id="yearTo" name="yearTo" class="input-small" />
 
       <div class="movie-type">
-        <label class="control-label" for="movieType">Types:</label>
-        <select class="movieType">
+        <label class="control-label" for="type">Types:</label>
+        <select id="type">
           <option value>--</option>
-          <option value="FILM">FILM</option>
-          <option value="TV_SHOW">TV_SHOW</option>
-          <option value="TV_SERIES">TV_SERIES</option>
-          <option value="MINI_SERIES">MINI_SERIES</option>
-          <option value="ALL">ALL</option>
+          <option value="FILM">ФИЛЬМЫ</option>
+          <option value="TV_SHOW">ТВ_ШОУ</option>
+          <option value="TV_SERIES">ТВ_СЕРИАЛЫ</option>
+          <option value="MINI_SERIES">МИНИ_СЕРИАЛЫ</option>
+          <option value="ALL">ВСЕ</option>
         </select>
       </div>
       
@@ -26,12 +26,12 @@ export const MOVIE_SEARCH_PANEL_TEMPLATE = `
       </div>
       
       <div class="order">
-        <label class="control-label" for="orderType">Order:</label>
-        <select class="orderType">
+        <label class="control-label" for="order">Order:</label>
+        <select id="order">
           <option value>--</option>
-          <option value="RATING">RATING</option>
-          <option value="NUM_VOTE">NUM_VOTE</option>
-          <option value="YEAR">YEAR</option>
+          <option value="RATING">РЕЙТИНГ</option>
+          <option value="NUM_VOTE">НОМЕР ГОЛОСОВ</option>
+          <option value="YEAR">ГОД</option>
         </select>
       </div>
       
